9fff753d1a6cb18a0bc0323215614e83905d5cc9
Pranav <pranavj648@gmail.com>
2022-01-08 19:51:16 -0500

    Added files and file changes

--- covid19_data_exploration.sql
+++ covid19_data_exploration.sql
@@ -1,5 +1,5 @@
 SELECT *
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 ORDER BY 3,4
 
 
@@ -11,7 +11,7 @@ ORDER BY 3,4
 
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 ORDER BY 1,2
 
 
@@ -20,7 +20,7 @@ ORDER BY 1,2
 -- Indicates COVID-19 fatality rates in Canada
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_percentage
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 WHERE location LIKE '%CANADA%'
 ORDER BY 1,2
 
@@ -29,7 +29,7 @@ ORDER BY 1,2
 -- Percentage of population infected with COVID-19
 
 SELECT location, date, population, total_cases, (total_cases/population)*100 AS percent_population_infected
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 --WHERE location LIKE '%CANADA%'
 ORDER BY 1,2
 
@@ -38,7 +38,7 @@ ORDER BY 1,2
 -- Countries with highest infection rates compared to their population
 
 SELECT location, population, MAX(total_cases) AS highest_infection_count, MAX((total_cases/population))*100 AS percent_population_infected
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 --WHERE location LIKE '%CANADA%'
 GROUP BY location, population
 ORDER BY percent_population_infected DESC
@@ -48,7 +48,7 @@ ORDER BY percent_population_infected DESC
 -- Countries with highest death count per population
 
 SELECT location, MAX(CAST(total_deaths AS INT)) AS total_death_count
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 --WHERE location LIKE '%CANADA%'
 WHERE continent IS NOT NULL
 GROUP BY location
@@ -59,7 +59,7 @@ ORDER BY total_death_count DESC
 -- Continents with highest death count per population
 
 SELECT continent, MAX(CAST(total_deaths AS INT)) AS total_death_count
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 --WHERE location LIKE '%CANADA%'
 WHERE continent IS NOT NULL
 GROUP BY continent
@@ -70,7 +70,7 @@ ORDER BY total_death_count DESC
 -- Global figures by date
 
 SELECT date, SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS deaths_percentage
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1,2
@@ -80,7 +80,7 @@ ORDER BY 1,2
 -- Total numbers
 
 SELECT SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS deaths_percentage
-FROM SQLProject1..covid_deaths
+FROM SQLProject..covid_deaths
 WHERE continent IS NOT NULL
 --GROUP BY date
 ORDER BY 1,2
@@ -90,8 +90,8 @@ ORDER BY 1,2
 -- Joining the 2 tables
 
 SELECT *
-FROM SQLProject1..covid_deaths dea
-JOIN SQLProject1..covid_vaccinations vac
+FROM SQLProject..covid_deaths dea
+JOIN SQLProject..covid_vaccinations vac
 ON dea.location = vac.location
 AND dea.date = vac.date
 
@@ -101,8 +101,8 @@ AND dea.date = vac.date
 -- Shows percentage of population that has recieved at least one vaccination
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-FROM SQLProject1..covid_deaths dea
-JOIN SQLProject1..covid_vaccinations vac
+FROM SQLProject..covid_deaths dea
+JOIN SQLProject..covid_vaccinations vac
 ON dea.location = vac.location
 AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
@@ -116,8 +116,8 @@ WITH POPVSVAC (continent, location, date, population, new_vaccinations, rolling_
 AS
 (
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location,dea.date) AS rolling_vaccinations --, (rolling_vaccinations/population)*100
-FROM SQLProject1..covid_deaths dea
-JOIN SQLProject1..covid_vaccinations vac
+FROM SQLProject..covid_deaths dea
+JOIN SQLProject..covid_vaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
@@ -145,8 +145,8 @@ INSERT INTO #PercentPopulationVaccinated
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.Location ORDER BY dea.location, dea.date) AS rolling_vaccinations
 --, (rolling_vaccinations/population)*100
-FROM SQLProject1..covid_deaths dea
-JOIN SQLProject1..covid_vaccinations vac
+FROM SQLProject..covid_deaths dea
+JOIN SQLProject..covid_vaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 --WHERE dea.continent IS NOT NULL
@@ -163,8 +163,42 @@ CREATE VIEW PercentPopulationVaccinated
 AS
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_vaccinations
 --, (rolling_vaccinations/population)*100
-FROM SQLProject1..covid_deaths dea
-JOIN SQLProject1..covid_vaccinations vac
+FROM SQLProject..covid_deaths dea
+JOIN SQLProject..covid_vaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
+
+
+
+
+--TABLEAU QUERIES
+
+1.
+SELECT SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS deaths_percentage
+FROM SQLProject..covid_deaths
+WHERE continent IS NOT NULL
+--GROUP BY date
+ORDER BY 1,2
+
+2.
+SELECT location, SUM(CAST(new_deaths AS INT)) AS total_death_count
+FROM SQLProject..covid_deaths
+WHERE continent IS NULL
+AND location NOT IN ('World', 'European Union', 'International')
+GROUP BY location
+ORDER BY total_death_count DESC
+
+3.
+SELECT location, population, MAX(total_cases) AS highest_infection_count, MAX((total_cases/population))*100 AS percent_population_infected
+FROM SQLProject..covid_deaths
+--WHERE location LIKE '%CANADA%'
+GROUP BY location, population
+ORDER BY percent_population_infected DESC
+
+4.
+SELECT location, population, date, MAX(total_cases) AS highest_infection_count, MAX((total_cases/population))*100 AS percent_population_infected
+FROM SQLProject..covid_deaths
+--WHERE location LIKE '%CANADA%'
+GROUP BY location, population, date
+ORDER BY percent_population_infected DESC
